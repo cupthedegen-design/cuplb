6b0a3984f342035e2484db9666a6c288710a6dc6
cupthedegen-design <cupthedegen@gmail.com>
2026-02-06 18:18:16 -0500

    Update leaderboard.js

--- api/leaderboard.js
+++ api/leaderboard.js
@@ -1,1 +1,10 @@
+import { supabase } from "./_supabase.js";
 
+export default async function handler(req, res) {
+  const { data } = await supabase
+    .from("leaderboard_users")
+    .select("*")
+    .order("rank", { ascending: true });
+
+  res.json(data);
+}
